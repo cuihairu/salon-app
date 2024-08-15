bff5c54e813e729a8537a49cfc0e9cc48e2ddf2a
cuihairu <chuihairu@gmail.com>
2024-08-15 15:03:53 +0800

    swiiper

--- src/pages/componets/home/index.tsx
+++ src/pages/componets/home/index.tsx
@@ -1,8 +1,39 @@
 import { View } from '@tarojs/components';
+import { Swiper } from '@nutui/nutui-react-taro'
+import {
+  CommonEventFunction,
+  SwiperProps as TaroSwiperProps,
+} from '@tarojs/components'
+
+const list = [
+  'https://storage.360buyimg.com/jdc-article/NutUItaro34.jpg',
+  'https://storage.360buyimg.com/jdc-article/NutUItaro2.jpg',
+  'https://storage.360buyimg.com/jdc-article/welcomenutui.jpg',
+  'https://storage.360buyimg.com/jdc-article/fristfabu.jpg',
+]
 
 function Index(){
+  const onChange: CommonEventFunction<TaroSwiperProps.onChangeEventDetail> = (
+    e
+  ) => {
+    console.log(`onChange is trigger ${e}`)
+  }
   return (
-    <View>欢迎来到首页！</View>
+    <View>
+      <Swiper defaultValue={1} autoPlay indicator onChange={onChange}>
+        {list.map((item, index) => (
+          <Swiper.Item key={item}>
+            <img
+              width='100%'
+              height='100%'
+              onClick={() => console.log(index)}
+              src={item}
+              alt=''
+            />
+          </Swiper.Item>
+        ))}
+      </Swiper>
+    </View>
   );
 }
 
